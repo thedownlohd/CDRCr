1862ca9de4c93da8304d07b0a13d88c2988666a5
thelohdown <kendall.loh@gmail.com>
2018-10-22 16:55:03 -0400

    hw05

---
+++ hw05/NewPackage.Rcheck/00_pkg_src/NewPackage/R/Area function.R
@@ -0,0 +1,14 @@
+#' Area Function
+#'
+#' This is an example function named 'area'
+#' which prints the area of a given rectangle.
+#'
+#' @param length A given length
+#' @param width A given width
+#' @return The product of \code{length} and \code{width}
+
+area <- function(length, width) {
+  length * width
+}
+
+area(3,4)
---
+++ hw05/NewPackage/data/data.R
@@ -0,0 +1,14 @@
+#' Average Heights and Weights for American Women
+#'
+#' This data provides a small sample of average heights and weights
+#' for American women aged 30–39 and some random data from the normal
+#' distribution.
+#'
+#' @usage{women}
+#' @format The women data frame contains 15 observations on 2 variables.
+#' \describe{
+#'   \item{height}{Height, in inches}
+#'   \item{weight}{Weight in lbs}
+#' }
+#' @source \url{http://www.worldalmanac.com/}
+"women"
